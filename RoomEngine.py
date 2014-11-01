0c0016c5d0e515e2b39ceacbf082326367b70ec8
Charlie Dawson <charlie@charlesddawson.com>
2014-11-01 14:35:00 -0400

    Minor changes: test of parsing

--- RoomEngine.py
+++ RoomEngine.py
@@ -1,7 +1,10 @@
 #Room Engine
 
+#import xml file
 import xml.etree.ElementTree as ET
 tree = ET.parse('adventure.xml')
 root = tree.getroot()
 
-rooms = root.findall("Room")
+rooms = root.findall("room")
+
+print rooms
